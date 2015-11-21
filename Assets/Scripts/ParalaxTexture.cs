5109bd2cbe1f4afca8b76d44ed97955e0c9ed4c4
Ryan Harrington <krooked590@gmail.com>
2015-11-21 14:27:06 -0800

    started merging main menu scene

--- Assets/Scripts/ParalaxTexture.cs
+++ Assets/Scripts/ParalaxTexture.cs
@@ -3,13 +3,20 @@ using System.Collections;
 
 public class ParalaxTexture : MonoBehaviour {
     public Transform target;
-    [Range(0f, 1f)]
     public float yPara = .5f;
-    [Range(0f, 1f)]
     public float xPara = .5f;
     public static int speedFactor = 1;
+    public bool useMouse = false;
+
+    private void Start() {
+        if(useMouse) {
+            target = new GameObject().transform;
+            target.position = Input.mousePosition;
+        }
+    }
 
     private void LateUpdate() {
+        if(useMouse) target.position = Input.mousePosition;
         var rend = GetComponent<MeshRenderer>();
         var mat = rend.material;
         mat.mainTextureOffset = new Vector2(target.position.x * (xPara / 100) * speedFactor, target.position.y * (yPara / 100) * speedFactor);
